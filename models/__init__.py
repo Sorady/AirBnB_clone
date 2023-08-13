624d05db7bdb102823ef7f5a2e4250f72f8d0d0a
Sorady <125706196+Sorady@users.noreply.github.com>
2023-08-14 01:48:55 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""Initialize the BaseModel class and the FileStorage Class """
-
+"""
+Initialize the BaseModel class and the FileStorage Class
+"""
 
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
