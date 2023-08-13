1aeb6967c99272f3f4c939be5cf5c766ad4dd941
Beshoy-Ayad <beshoy.ayad@gmail.com>
2023-08-13 21:15:55 +0000

    7th

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -5,60 +5,39 @@
 import unittest
 from datetime import datetime
 from models.base_model import BaseModel
+from models.engine import file_storage
+import json
+import os
 
 
-class TestBaseModel(unittest.TestCase):
-    """Test cases for BaseModel class"""
+FileStorage = file_storage.FileStorage
+classes = {"BaseModel": BaseModel, }
 
-    def setUp(self):
-        """Set up the test environment"""
-        self.base_model = BaseModel()
-
-    def test_instance_attributes(self):
-        """Test instance attributes"""
-        self.assertTrue(hasattr(self.base_model, 'id'))
-        self.assertTrue(hasattr(self.base_model, 'created_at'))
-        self.assertTrue(hasattr(self.base_model, 'updated_at'))
-
-    def test_instance_methods(self):
-        """Test instance methods"""
-        self.assertTrue(hasattr(self.base_model, '__str__'))
-        self.assertTrue(hasattr(self.base_model, 'save'))
-        self.assertTrue(hasattr(self.base_model, 'to_dict'))
-
-    def test_str_method(self):
-        """Test __str__ method"""
-        string_representation = "[BaseModel] ({}) {}".format(
-            self.base_model.id, self.base_model.__dict__)
-        self.assertEqual(string_representation, str(self.base_model))
-
-    def test_save_method(self):
-        """Test save method"""
-        original_updated_at = self.base_model.updated_at
-        self.base_model.save()
-        self.assertNotEqual(original_updated_at, self.base_model.updated_at)
-
-    def test_to_dict_method(self):
-        """Test to_dict method"""
-        to_dict_result = self.base_model.to_dict()
-        self.assertEqual(type(to_dict_result), dict)
-        self.assertIn('__class__', to_dict_result)
-        self.assertIn('created_at', to_dict_result)
-        self.assertIn('updated_at', to_dict_result)
-        self.assertIn('id', to_dict_result)
-
-    def test_to_dict_updated_at(self):
-        """Test if updated_at is ISO formatted"""
-        to_dict_result = self.base_model.to_dict()
-        updated_at = self.base_model.updated_at.isoformat()
-        self.assertEqual(to_dict_result['updated_at'], updated_at)
-
-    def test_to_dict_created_at(self):
-        """Test if created_at is ISO formatted"""
-        to_dict_result = self.base_model.to_dict()
-        created_at = self.base_model.created_at.isoformat()
-        self.assertEqual(to_dict_result['created_at'], created_at)
+class TestFileStorage(unittest.TestCase):
+    """Tests for the FileStorage Class"""
+    def test_all_function(self):
+        """Tests for the all fuction that it returns the dictionary
+        """
+        storage = file_storage.FileStorage()
+        all_objects = storage.all()
+        self.assertIsInstance(all_objects, dict)
 
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_save_function(self):
+        """Test that save properly saves objects to file.json"""
+        storage = FileStorage()
+        new_dict = {}
+        for key, value in classes.items():
+            instance = value()
+            instance_key = instance.__class__.__name__ + "." + instance.id
+            new_dict[instance_key] = instance
+        save = FileStorage._FileStorage__objects
+        FileStorage._FileStorage__objects = new_dict
+        storage.save()
+        FileStorage._FileStorage__objects = save
+        for key, value in new_dict.items():
+            new_dict[key] = value.to_dict()
+        string = json.dumps(new_dict)
+        with open("file.json", "r") as f:
+            js = f.read()
+        self.assertEqual(json.loads(string), json.loads(js))
