017a9f33f1a6256020425f061c8c939383844308
Beshoy-Ayad <beshoy.ayad@gmail.com>
2023-08-13 21:09:28 +0000

    6th

--- models/__init__.py
+++ models/__init__.py
@@ -3,6 +3,7 @@
 The module is responsible for creating an instance of the FileStorage class that can be used to store files in the application.
 """"
 
+from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
 
